004a64a62ea4d795a1e39b36f9e93d0a45c97791
steve <sylvius4@gmail.com>
2024-08-04 21:59:35 +0900

    add: sort

---
+++ lv2/06_[unsolved]game-maps(bfs).js
@@ -0,0 +1,2 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/1844
+// 게임 맵 최단거리
---
+++ lv2/07_[unsolved]fatigue(dfs).js
@@ -0,0 +1,3 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/87946
+// 피로도
+// 완전탐색 vs DFS + 백트래킹
