8ba52925e028dea79cb2149f09481aee3abc6a71
steve <sylvius4@gmail.com>
2024-08-06 01:29:51 +0900

    add:heap sorting solution

---
+++ lv2/12_[solution]heap-sorting.js
@@ -0,0 +1,95 @@
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    getParentIndex(index) {
+        return Math.floor((index - 1) / 2);
+    }
+
+    getLeftChildIndex(index) {
+        return 2 * index + 1;
+    }
+
+    getRightChildIndex(index) {
+        return 2 * index + 2;
+    }
+
+    swap(index1, index2) {
+        [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
+    }
+
+    push(value) {
+        this.heap.push(value);
+        this.heapifyUp();
+    }
+
+    heapifyUp() {
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            let parentIndex = this.getParentIndex(index);
+            if (this.heap[parentIndex] > this.heap[index]) {
+                this.swap(parentIndex, index);
+                index = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    pop() {
+        if (this.heap.length === 1) return this.heap.pop();
+
+        const root = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.heapifyDown(0);
+
+        return root;
+    }
+
+    heapifyDown(index) {
+        let smallest = index;
+        const leftChildIndex = this.getLeftChildIndex(index);
+        const rightChildIndex = this.getRightChildIndex(index);
+
+        if (leftChildIndex < this.heap.length && this.heap[leftChildIndex] < this.heap[smallest]) {
+            smallest = leftChildIndex;
+        }
+
+        if (rightChildIndex < this.heap.length && this.heap[rightChildIndex] < this.heap[smallest]) {
+            smallest = rightChildIndex;
+        }
+
+        if (smallest !== index) {
+            this.swap(index, smallest);
+            this.heapifyDown(smallest);
+        }
+    }
+
+    peek() {
+        return this.heap[0];
+    }
+
+    size() {
+        return this.heap.length;
+    }
+}
+
+function solution(scoville, K) {
+    const minHeap = new MinHeap();
+    scoville.forEach(s => minHeap.push(s));
+
+    let mixCount = 0;
+
+    while (minHeap.size() > 1 && minHeap.peek() < K) {
+        const first = minHeap.pop();
+        const second = minHeap.pop();
+
+        const newScoville = first + second * 2;
+        minHeap.push(newScoville);
+
+        mixCount++;
+    }
+
+    return minHeap.peek() >= K ? mixCount : -1;
+}
